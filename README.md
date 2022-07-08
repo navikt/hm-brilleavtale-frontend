0530cd9b46dc388f25be5314033552193f516f0b
Erik Eiesland <erik@eies.land>
2022-07-08 14:11:03 +0200

    trigger workflow

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # hm-brilleavtale-frontend
+
+## Kom i gang
+
+Installer alle avhengigheter:
+```bash
+$ npm install
+```
--- client/src/mocks/browser.ts
+++ client/src/mocks/browser.ts
@@ -1,5 +1,5 @@
-import { RequestHandler, setupWorker } from "msw";
+import { RequestHandler, setupWorker } from 'msw'
 
-const handlers: RequestHandler[] = [];
+const handlers: RequestHandler[] = []
 
-export const worker = setupWorker(...handlers);
+export const worker = setupWorker(...handlers)
