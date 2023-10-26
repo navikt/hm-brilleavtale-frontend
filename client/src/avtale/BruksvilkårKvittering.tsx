bbee9d9ddd1e15636c3059c9b4644d35a729b8fc
Aasmund Nordstoga <aasmund.nordstoga@nav.no>
2023-10-26 14:35:50 +0200

    Info om API/leverandør på kvittering for godkjende bruksvilkår for API

--- client/src/avtale/BruksvilkårKvittering.tsx
+++ client/src/avtale/BruksvilkårKvittering.tsx
@@ -1,11 +1,9 @@
-import { Alert, BodyLong, Heading } from '@navikt/ds-react'
-import { Trans, useTranslation } from 'react-i18next'
-import { Link, useLocation } from 'react-router-dom'
-import { AppLink } from '../components/AppLink'
-import { Avstand } from '../components/Avstand'
-import { DsLink } from '../components/DsLink'
-import { Virksomhet } from '../types'
-import { AvtalePanel } from './AvtalePanel'
+import {Alert, BodyLong, Heading} from '@navikt/ds-react'
+import {Trans, useTranslation} from 'react-i18next'
+import {Link, useLocation} from 'react-router-dom'
+import {Avstand} from '../components/Avstand'
+import {DsLink} from '../components/DsLink'
+import {Virksomhet} from '../types'
 import ScrollToTop from "../components/ScrollToTop";
 import {BruksvilkårPanel} from "./BruksvilkårPanel";
 
@@ -22,6 +20,8 @@ export function BruksvilkårKvittering() {
         {t('avtale.kvittering_for', { navn: virksomhet.navn })}
       </Heading>
       <Alert variant="success">{t('avtale_utvidet.suksess')}</Alert>
+        <Avstand marginBottom={5}></Avstand>
+      <Alert variant="info">{t('avtale_utvidet.informasjon')}</Alert>
       <Avstand marginBottom={5} />
       <BodyLong spacing>
         <Trans t={t} i18nKey="avtale.mer_informasjon">
